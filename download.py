bdcc399df5f342fd9248c5ce74ed730645facc09
Fernando Martinez <frndmartinezglez@gmail.com>
2019-12-20 18:22:48 -0500

    Implement episodes download with iterators

--- download.py
+++ download.py
@@ -2,6 +2,8 @@ import requests
 import os
 import bs4
 
+from itertools import chain
+
 all_episodes = []
 webtoonurl = 'https://www.webtoons.com'
 baseurl = 'https://www.webtoons.com/en/action/the-god-of-high-school/'
@@ -14,33 +16,33 @@ title = '66'
 extra = 'list?title_no='+title
 
 
-def get_all_episodes(url):
-    pages_list = []
+def get_episodes_list(url):
     res = requests.get(url)
     res.raise_for_status()
+
     soup = bs4.BeautifulSoup(res.text, 'lxml')
+
     episode_list = soup.find('ul', id='_listUl')
+    for href in (a.get('href') for a in episode_list.find_all('a')):
+        yield href
+
+
+def get_pages(url):
+    res = requests.get(url)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
+
     paginator = soup.find('div', class_='paginate')
-    for i in (a.get('href') for a in paginator.find_all('a')):
-        if i != '#':
-            res = requests.get(webtoonurl + i)
-            print(webtoonurl + i)
-            res.raise_for_status()
-            soup = bs4.BeautifulSoup(res.text, 'lxml')
-            episode_list = soup.find('ul', id='_listUl')
-            episode_list = (a.get('href') for a in episode_list.find_all('a'))
-            # print(list(episode_list))
-        yield episode_list
+
+    for href in (href.get('href') for href in paginator.find_all('a')):
+        if href == '#':
+            continue
+        yield href
+
     if paginator.find('a', class_='pg_next'):
-
-        for i in get_all_episodes(webtoonurl + paginator.find('a',
-                                                              class_='pg_next').get('href')):
-            yield i
-
-
-def lazy_iterator(n=0):
-    for i in range(n):
-        yield i
+        href = paginator.find('a', class_='pg_next').get('href')
+        return get_pages(f'{webtoonurl}{href}')
 
 
 def prune_list(episode_list):
@@ -88,11 +90,23 @@ def download_episode(url, episode):
 
 
 if __name__ == "__main__":
-    # latest_episode, diff = get_episode_diff(baseurl+extra)
-            # global all_episodes
-    print('Getting all episodes\n')
-    fernan = list(get_all_episodes(baseurl+extra))
-    print('Blame', fernan)
+    pages = map(
+        lambda href: f'{webtoonurl}{href}',
+        get_pages(baseurl + extra),
+    )
+
+    episodes = chain.from_iterable(
+        map(get_episodes_list, pages),
+    )
+
+    # you can get the full list of episodes with
+    es = set(episodes)
+
+    # or lazy iterate with
+    for e in episodes:
+        print(e)
+
+    # print('Blame', list(episodes))
     # print(all_episodes)
     # all_episodes = prune_list(all_episodes)
     # print(all_episodes)
