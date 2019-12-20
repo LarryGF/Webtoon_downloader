3eda6c994fb46ed405c4368eba5cff173fa4c729
LarryGF <larrywtf609@gmail.com>
2019-12-20 00:26:52 -0500

    Making it check for differences in episodes

--- download.py
+++ download.py
@@ -4,19 +4,44 @@ import bs4
 
 url_list = []
 base_url = 'https://www.webtoons.com/en/action/the-god-of-high-school/ep-440/viewer?title_no=66&episode_no=443'
+baseurl = 'https://www.webtoons.com/en/action/the-god-of-high-school/'
+extra = 'list?title_no=66'
 
-res = requests.get(base_url)
-res.raise_for_status()
 
-soup = bs4.BeautifulSoup(res.text)
+def get_episode_diff(url):
+    print('Getting episode difference')
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
+    episode_list = soup.find('ul', id='_listUl')
+    latest_episode_url = episode_list.find_all('li')[0].find('a').get('href')
+    latest_episode = int(latest_episode_url.split('/')[6][3:])
+    latest_real_episode = int(latest_episode_url.split(
+        '/')[7].split('&')[-1][len('episode_no='):])
+    episode_diff = latest_real_episode - latest_episode
+    return (latest_episode, episode_diff)
 
-img_list = soup.select('img._images')
 
-for image in img_list:
-    url_list.append(image.get('data-url'))
+def download_episode(url):
+    print('Downloading: '+url)
+    res = requests.get(url)
+    res.raise_for_status()
 
-for index, img_url in enumerate(url_list):
-    r = requests.get(img_url, headers={'Referer': base_url})
-    file = open(str(index)+'.JPEG', 'wb')
-    file.write(r.content)
-    file.close()
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
+
+    img_list = soup.select('img._images')
+
+    for image in img_list:
+        url_list.append(image.get('data-url'))
+
+    for index, img_url in enumerate(url_list):
+        r = requests.get(img_url, headers={'Referer': url})
+        file = open(str(index)+'.JPEG', 'wb')
+        file.write(r.content)
+        file.close()
+
+
+if __name__ == "__main__":
+    latest_episode, diff = get_episode_diff(baseurl+extra)
+    download_episode(
+        baseurl+'ep-{}/viewer?title_no=66&episode_no={}'.format(latest_episode, latest_episode+diff))
