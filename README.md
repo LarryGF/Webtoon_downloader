df9f70d4f585174c6ce6314e6ac13bbf509f60f8
LarryGF <larrywtf609@gmail.com>
2019-12-20 22:56:01 -0500

    Added download folder option and created README

---
+++ README.md
@@ -0,0 +1,31 @@
+# WEBTOON DOWNLOADER
+
+## Summary
+
+This scripts downloads full webtoons from [Naver Webtoon](https://www.webtoons.com). It keeps them sorted in folders in the way: `{download folder}/{webtoon title}/{episode}` for easier browsing.
+
+## Usage
+
+- First install the necessary requirements:
+  ```
+  pip install -r requirements.txt
+  ```
+- After that run the `download` script:
+
+  ```
+  python download {webtoon url} {title number} {download folder}
+  ```
+
+  You can check for the `webtoon url` and the `title number` in the url of the webtoon you want to download, just go to [Naver Webtoon](https://www.webtoons.com) and search for the webtoon you want and copy its' _url_. It should be something like this: `https://www.webtoons.com/en/action/the-god-of-high-school/list?title_no=66`. As you can see the _url_ is in the form: `https://www.webtoons.com/{language}/{genre}/{webtoon title}/list?title_no={title number}`. The `webtoon url` is everything up to the `webtoon title`, in this case it would be: `https://www.webtoons.com/en/action/the-god-of-high-school/`, and the `title number` would be `66`.
+
+  ## Important notice
+
+  In case you didn't read this README and just ran the script, every parameter has a default value:
+
+  - **webtoon url**: https://www.webtoons.com/en/fantasy/tower-of-god/
+  - **title number**: 95
+  - **download folder**: ./
+
+## Thanks
+
+Special thanks to @frndmg for his help on fetching all episodes' urls in a smarter, simpler way.
--- download.py
+++ download.py
@@ -8,8 +8,8 @@ all_episodes = []
 webtoonurl = 'https://www.webtoons.com'
 
 
-def get_base_values(url, number):
-    return url, number
+def get_base_values(url='https://www.webtoons.com/en/fantasy/tower-of-god/', number='95', download_folder='./'):
+    return url, number, download_folder
 
 
 def get_episodes(url):
@@ -40,20 +40,6 @@ def get_pages(url):
         href = paginator.find('a', class_='pg_next').get('href')
         return get_pages(f'{webtoonurl}{href}')
 
-# def get_episode_diff(url):
-#     print('Getting episode difference')
-#     res = requests.get(url)
-#     res.raise_for_status()
-#     soup = bs4.BeautifulSoup(res.text, 'lxml')
-#     episode_list = soup.find('ul', id='_listUl')
-#     latest_episode_url = episode_list.find_all('li')[0].find('a').get('href')
-#     latest_episode = int(latest_episode_url.split('/')[6][3:])
-#     latest_real_episode = int(latest_episode_url.split(
-#         '/')[7].split('&')[-1][len('episode_no='):])
-#     episode_diff = latest_real_episode - latest_episode
-#     print('Episode difference is '+str(episode_diff)+'\n')
-#     return (latest_episode, episode_diff)
-
 
 def download_episode(url):
     url_list = []
@@ -81,15 +67,13 @@ def download_episode(url):
 
 
 if __name__ == "__main__":
-    baseurl, title = fire.Fire(get_base_values)
+    baseurl, title, download_folder = fire.Fire(get_base_values)
     extra = 'list?title_no='+str(title)
 
     if baseurl.endswith('/'):
         name = baseurl.split('/')[-2]
     else:
         name = baseurl.split('/')[-1]
-    print(baseurl)
-    print(title)
 
     pages = map(
         lambda href: f'{webtoonurl}{href}',
@@ -103,7 +87,7 @@ if __name__ == "__main__":
     # you can get the full list of episodes with
     # es = set(episodes)
 
-    webtoon_dir = name.replace('-', ' ')
+    webtoon_dir = os.path.join(download_folder, name.replace('-', ' '))
     os.makedirs(webtoon_dir, exist_ok=True)
     print('Created ', webtoon_dir, '\n')
 
