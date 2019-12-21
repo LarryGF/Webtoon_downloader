4e307b1504f1af6d5f744c84772d2b95c2abc3ec
LarryGF <larrywtf609@gmail.com>
2019-12-20 21:31:34 -0500

    Downloading again

--- download.py
+++ download.py
@@ -44,27 +44,22 @@ def get_pages(url):
         href = paginator.find('a', class_='pg_next').get('href')
         return get_pages(f'{webtoonurl}{href}')
 
-
-def prune_list(episode_list):
-    episode_list = episode_list.sort()
-
-
-def get_episode_diff(url):
-    print('Getting episode difference')
-    res = requests.get(url)
-    res.raise_for_status()
-    soup = bs4.BeautifulSoup(res.text, 'lxml')
-    episode_list = soup.find('ul', id='_listUl')
-    latest_episode_url = episode_list.find_all('li')[0].find('a').get('href')
-    latest_episode = int(latest_episode_url.split('/')[6][3:])
-    latest_real_episode = int(latest_episode_url.split(
-        '/')[7].split('&')[-1][len('episode_no='):])
-    episode_diff = latest_real_episode - latest_episode
-    print('Episode difference is '+str(episode_diff)+'\n')
-    return (latest_episode, episode_diff)
+# def get_episode_diff(url):
+#     print('Getting episode difference')
+#     res = requests.get(url)
+#     res.raise_for_status()
+#     soup = bs4.BeautifulSoup(res.text, 'lxml')
+#     episode_list = soup.find('ul', id='_listUl')
+#     latest_episode_url = episode_list.find_all('li')[0].find('a').get('href')
+#     latest_episode = int(latest_episode_url.split('/')[6][3:])
+#     latest_real_episode = int(latest_episode_url.split(
+#         '/')[7].split('&')[-1][len('episode_no='):])
+#     episode_diff = latest_real_episode - latest_episode
+#     print('Episode difference is '+str(episode_diff)+'\n')
+#     return (latest_episode, episode_diff)
 
 
-def download_episode(url, episode):
+def download_episode(url):
     url_list = []
 
     print('Downloading episode: '+url+'\n')
@@ -79,7 +74,7 @@ def download_episode(url, episode):
     for image in img_list:
         url_list.append(image.get('data-url'))
 
-    for index, img_url in enumerate(url_list[:2]):
+    for index, img_url in enumerate(url_list):
         print('\t Downloading '+img_url)
         r = requests.get(img_url, headers={'Referer': url})
         file = open(os.path.join(episode_dir, str(index)+'.JPEG'), 'wb')
@@ -102,22 +97,18 @@ if __name__ == "__main__":
     # you can get the full list of episodes with
     # es = set(episodes)
 
+    webtoon_dir = name.replace('-', ' ')
+    os.makedirs(webtoon_dir, exist_ok=True)
+    print('Created ', webtoon_dir, '\n')
+
     # or lazy iterate with
     for e in episodes:
+        episode_dir = os.path.join(
+            webtoon_dir, str(e.split('/')[7].split('&')[-1]))
+        os.makedirs(episode_dir, exist_ok=True)
+        print('Created '+episode_dir+'\n')
+        download_episode(url=e)
         print(e)
 
-    # print('Blame', list(episodes))
-    # print(all_episodes)
-    # all_episodes = prune_list(all_episodes)
-    # print(all_episodes)
-    # webtoon_dir = name.replace('-', ' ')
-    # os.makedirs(webtoon_dir, exist_ok=True)
-    # print('Created ' + webtoon_dir+'\n')
     # while latest_episode > 0:
-    #     episode_dir = os.path.join(
-    #         webtoon_dir, str(latest_episode))
-    #     os.makedirs(episode_dir, exist_ok=True)
-    #     print('Created '+episode_dir+'\n')
-    #     download_episode(url=baseurl+'ep-{}/viewer?{}&episode_no={}'.format(
-    #         latest_episode, title, latest_episode+diff), episode=latest_episode)
     #     latest_episode -= 1
