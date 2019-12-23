7d81fe72f84172daa927d5ceaeb96026fc4e4994
LarryGF <larrywtf609@gmail.com>
2019-12-23 01:01:03 -0500

    Added naive way of not redownloading images

--- download.py
+++ download.py
@@ -2,6 +2,7 @@ import requests
 import os
 import bs4
 import fire
+import json
 from itertools import chain
 
 webtoonurl = 'https://www.webtoons.com'
@@ -39,11 +40,10 @@ def get_pages(url):
         return get_pages(f'{webtoonurl}{href}')
 
 
-def download_episode(url):
+def download_episode(url: str, db: dict, name: str, chapter: str):
     url_list = []
 
     print('Downloading episode: '+url+'\n')
-
     res = requests.get(url)
     res.raise_for_status()
 
@@ -55,18 +55,31 @@ def download_episode(url):
         url_list.append(image.get('data-url'))
 
     for index, img_url in enumerate(url_list):
+        if db[name][chapter].setdefault(img_url, False):
+            print(str(index)+'.JPEG ', 'has already been downloaded')
+            continue
+
         print('\t Downloading '+img_url)
         r = requests.get(img_url, headers={'Referer': url})
         file = open(os.path.join(episode_dir, str(index)+'.JPEG'), 'wb')
         print('\t Saving image in: ' +
               str(os.path.join(episode_dir, str(index)+'.JPEG'))+'\n')
+
         file.write(r.content)
         file.close()
+        db[name][chapter][img_url] = True
+        db_file = open('db.json', 'w')
+        json.dump(db, db_file)
+        db_file.close()
 
 
 if __name__ == "__main__":
     baseurl, title, download_folder = fire.Fire(get_base_values)
     extra = 'list?title_no='+str(title)
+    if not os.path.exists('db.json'):
+        webtoon_db = {}
+    else:
+        webtoon_db = json.load(open('db.json'))
 
     if baseurl.endswith('/'):
         name = baseurl.split('/')[-2]
@@ -82,7 +95,7 @@ if __name__ == "__main__":
     episodes = chain.from_iterable(
         map(get_episodes, pages),
     )
-
+    webtoon_db.setdefault(name.replace('-', ' '), {})
     # you can get the full list of episodes with
     # es = set(episodes)
 
@@ -92,8 +105,12 @@ if __name__ == "__main__":
 
     # or lazy iterate with
     for e in episodes:
+        episode = str(e.split('/')[7].split('&')[-1][len('episode_no='):])
+        webtoon_db[name.replace('-', ' ')].setdefault(episode, {})
+
         episode_dir = os.path.join(
-            webtoon_dir, str(e.split('/')[7].split('&')[-1][len('episode_no='):]))
+            webtoon_dir, episode)
         os.makedirs(episode_dir, exist_ok=True)
         print('Created '+episode_dir+'\n')
-        download_episode(url=e)
+        download_episode(url=e, db=webtoon_db,
+                         name=name.replace('-', ' '), chapter=episode)
