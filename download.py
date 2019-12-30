28549e1ebb433e667900f960f803399d6e5e3df2
LarryGF <larrywtf609@gmail.com>
2019-12-30 14:37:54 -0500

    Generating genres.json

--- download.py
+++ download.py
@@ -6,6 +6,40 @@ import json
 from itertools import chain
 
 webtoonurl = 'https://www.webtoons.com'
+genreurl = 'https://www.webtoons.com/en/genre'
+
+thumbnails_dir = 'thumbnails'
+
+
+def get_all_comics_by_genre():
+    os.makedirs(thumbnails_dir, exist_ok=True)
+    res = requests.get(genreurl)
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
+    contents = soup.find('div', class_='card_wrap genre').contents
+    count = contents.count('\n')
+    i = 0
+    while i < count:
+        contents.remove('\n')
+        i += 1
+    genres = {}
+    for index, i in enumerate(contents):
+        if index % 2 == 1:
+            info_dict = {}
+            li_list = i.find_all('li')
+            for li in li_list:
+                title = li.find('p', class_="subj").text
+                info_dict.setdefault(title, {})
+                info_dict[title]['href'] = li.find('a').get('href')
+                info_dict[title]['author'] = li.find(
+                    'p', class_="author").text
+                info_dict[title]['likes'] = li.find(
+                    'em', class_="grade_num").text
+                info_dict[title]['img_url'] = li.find('img').get('src')
+                info_dict[title]['img_path'] = os.path.join(
+                    thumbnails_dir, title+'.jpg')
+
+            genres[contents[index-1].text] = info_dict
+    json.dump(genres, open('genres.json', 'w'), indent=2)
 
 
 def get_base_values(url='https://www.webtoons.com/en/fantasy/tower-of-god/', number='95', download_folder='./'):
@@ -74,43 +108,45 @@ def download_episode(url: str, db: dict, name: str, chapter: str):
 
 
 if __name__ == "__main__":
-    baseurl, title, download_folder = fire.Fire(get_base_values)
-    extra = 'list?title_no='+str(title)
-    if not os.path.exists('db.json'):
-        webtoon_db = {}
-    else:
-        webtoon_db = json.load(open('db.json'))
+    fire.Fire()
+    episode_dir = ''
+    # baseurl, title, download_folder = fire.Fire(get_base_values)
+    # extra = 'list?title_no='+str(title)
+    # if not os.path.exists('db.json'):
+    #     webtoon_db = {}
+    # else:
+    #     webtoon_db = json.load(open('db.json'))
 
-    if baseurl.endswith('/'):
-        name = baseurl.split('/')[-2]
-    else:
-        name = baseurl.split('/')[-1]
-        baseurl = baseurl + '/'
+    # if baseurl.endswith('/'):
+    #     name = baseurl.split('/')[-2]
+    # else:
+    #     name = baseurl.split('/')[-1]
+    #     baseurl = baseurl + '/'
 
-    pages = map(
-        lambda href: f'{webtoonurl}{href}',
-        get_pages(baseurl + extra),
-    )
+    # pages = map(
+    #     lambda href: f'{webtoonurl}{href}',
+    #     get_pages(baseurl + extra),
+    # )
 
-    episodes = chain.from_iterable(
-        map(get_episodes, pages),
-    )
-    webtoon_db.setdefault(name.replace('-', ' '), {})
-    # you can get the full list of episodes with
-    # es = set(episodes)
+    # episodes = chain.from_iterable(
+    #     map(get_episodes, pages),
+    # )
+    # webtoon_db.setdefault(name.replace('-', ' '), {})
+    # # you can get the full list of episodes with
+    # # es = set(episodes)
 
-    webtoon_dir = os.path.join(download_folder, name.replace('-', ' '))
-    os.makedirs(webtoon_dir, exist_ok=True)
-    print('Created ', webtoon_dir, '\n')
+    # webtoon_dir = os.path.join(download_folder, name.replace('-', ' '))
+    # os.makedirs(webtoon_dir, exist_ok=True)
+    # print('Created ', webtoon_dir, '\n')
 
-    # or lazy iterate with
-    for e in episodes:
-        episode = str(e.split('/')[7].split('&')[-1][len('episode_no='):])
-        webtoon_db[name.replace('-', ' ')].setdefault(episode, {})
+    # # or lazy iterate with
+    # for e in episodes:
+    #     episode = str(e.split('/')[7].split('&')[-1][len('episode_no='):])
+    #     webtoon_db[name.replace('-', ' ')].setdefault(episode, {})
 
-        episode_dir = os.path.join(
-            webtoon_dir, episode)
-        os.makedirs(episode_dir, exist_ok=True)
-        print('Created '+episode_dir+'\n')
-        download_episode(url=e, db=webtoon_db,
-                         name=name.replace('-', ' '), chapter=episode)
+    #     episode_dir = os.path.join(
+    #         webtoon_dir, episode)
+    #     os.makedirs(episode_dir, exist_ok=True)
+    #     print('Created '+episode_dir+'\n')
+    #     download_episode(url=e, db=webtoon_db,
+    #                      name=name.replace('-', ' '), chapter=episode)
